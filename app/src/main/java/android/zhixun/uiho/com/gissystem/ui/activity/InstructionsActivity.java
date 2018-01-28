b85f60e3a3f593b444890f1b295f291e315cc664
simplepeng <383559698@qq.com>
2018-01-29 00:24:52 +0800

    使用说明 webview

--- app/src/main/java/android/zhixun/uiho/com/gissystem/ui/activity/InstructionsActivity.java
+++ app/src/main/java/android/zhixun/uiho/com/gissystem/ui/activity/InstructionsActivity.java
@@ -2,6 +2,10 @@ package android.zhixun.uiho.com.gissystem.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
 import android.zhixun.uiho.com.gissystem.R;
 
 import com.yibogame.flux.stores.Store;
@@ -12,6 +16,9 @@ import com.yibogame.flux.stores.Store;
 
 public class InstructionsActivity extends BaseActivityWithTitle {
 
+    private WebView mWebView;
+    private ProgressBar mProgressBar;
+
     @Override
     protected int getLayoutRes() {
         return R.layout.activity_instructions;
@@ -20,6 +27,37 @@ public class InstructionsActivity extends BaseActivityWithTitle {
     @Override
     protected void onCreateActivity(@Nullable Bundle savedInstanceState) {
         setTitleText("使用说明");
+        mWebView = findViewById(R.id.webview);
+        mProgressBar = findViewById(R.id.progressBar);
+
+        mWebView.loadUrl("http://www.map023.cn:8080/moveImg/index.html");
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    if (mProgressBar.getVisibility() == View.GONE)
+                        mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+        });
+//        mWebView.setWebViewClient(new WebViewClient(){
+//
+//            @Override
+//            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+//                super.onPageStarted(view, url, favicon);
+//                mProgressBar.setVisibility(View.VISIBLE);
+//            }
+//
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                super.onPageFinished(view, url);
+//                mProgressBar.setVisibility(View.GONE);
+//            }
+//        });
     }
 
 
